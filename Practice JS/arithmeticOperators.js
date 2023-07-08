f58859c294bc2082ef2c07bc5a190e5ab1b0247a
Nauman Farooq <138984368+whizzNauman@users.noreply.github.com>
2023-07-09 01:37:20 +0500

    Folder Added of Practice JavaScript
    The newly created folder will periodically be updated with JavaScript practice files and code.

---
+++ Practice JS/arithmeticOperators.js
@@ -0,0 +1,26 @@
+/*=========Mulitline Comments=========*/
+/* Arithmetic Operators in JS:
+    + addition
+    - subtraction
+    * multiplication
+    / division
+*/
+
+/* Comparision Operators in JS:
+    > greater than
+    < less than
+    == equal
+*/
+
+/*=========Single Comments=========*/
+
+// console.log(2 + 2);
+// console.log(1 + 2 + 3 + 4 + 5);
+// console.log(20 - 18);
+// console.log(2 * 3);
+// console.log(8 * 1);
+// console.log(8 / 1);
+// console.log(3 > 2);
+// console.log(2 < 3);
+// console.log(10 == 10);
+
