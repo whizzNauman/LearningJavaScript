9c9ee767e5aeda73d9ecf5d7e17bd4b5b772e67a
Nauman Farooq <138984368+whizzNauman@users.noreply.github.com>
2023-07-10 04:39:35 +0500

    Exercise  code has been updated
    In this code some exercises has been completed seccessfully.

--- Practice JS/arithmeticOperators.js
+++ Practice JS/arithmeticOperators.js
@@ -24,3 +24,50 @@
 // console.log(2 < 3);
 // console.log(10 == 10);
 
+/*========= += Operator =========*/
+
+// The concatenation assignment operator, +=
+// This operator's syntax is exactly the same as the addition assignment operator. The difference is in the data type used:
+
+var longString = "";
+longString += "Once";
+longString += " upon";
+longString += " a";
+longString += " time";
+longString += "...";
+console.log(longString); // "Once upon a time..."
+
+/*========= Task 2 =========*/
+
+var timeRemaining = 4;
+var energy = 10;
+console.log("Game over: " + (timeRemaining == 0 || energy == 0));
+
+/*========= Task 3 =========*/
+
+var num1 = 2;
+var num2 = 5;
+var test1 = num1 % 2;
+var test2 = num2 % 2;
+var result1 = test1 == 0;
+var result2 = test2 == 0;
+console.log("Is", num1, "an even number?", result1);
+console.log("Is", num2, "an even number?", result2)
+
+/*========= Task 4 =========*/
+
+console.log(5 + 10);
+
+/*========= Task 5 =========*/
+
+var now = 'Now in ';
+var three = 3;
+var d = 'D!';
+console.log(now + three + d);
+
+/*========= Task 6 =========*/
+
+var counter = 0;
+counter += 5;
+counter += 3;
+console.log(counter);
